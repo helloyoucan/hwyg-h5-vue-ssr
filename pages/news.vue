2fe2bc8f6f6ad25600c08cf2f8f6197fa8d17256
helloyoucan <helloyoucan@163.com>
2019-09-06 17:48:58 +0800

    update

--- pages/news.vue
+++ pages/news.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>news</div>
+  <div>
+    news
+    <ul>
+      <li v-for="(item) in listData" :key="item" @click="handler(item)">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
-  layout: 'triplRrow'
+  layout: 'triplRrow',
+  data () {
+    return {
+      listData: (new Array(10000)).fill(0).map((_, index) => index)
+    }
+  },
+  methods: {
+    handler (index) {
+      console.log(index)
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
